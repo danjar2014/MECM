ca27350b973bed74f261eba0723405d5f5af6f3d
BMConsulting <belhassen.mbarki@gmail.com>
2025-11-18 10:00:44 +0100

    Add files via upload

---
+++ content-location-Applications.ps1
@@ -0,0 +1,46 @@
+﻿
+Write-Host "Querying Applications..." -ForegroundColor Cyan
+
+$Apps = Get-CMApplication
+$AppNames = $Apps.LocalizedDisplayName
+$AppCount = $AppNames.Count
+$RowCount = 1
+
+Write-Host "Returned $AppCount objects"
+
+foreach ($AppName in $AppNames) {
+  Write-Host "Application: $AppName ($RowCount of $AppCount)" -ForegroundColor White
+  $DtNames = Get-CMDeploymentType -ApplicationName $AppName
+  Write-Host "Querying Deployment types..." -ForegroundColor Cyan
+  foreach ($dt in $DtNames) {
+    $DtSDMPackageXML = $dt.SDMPackageXML
+    $DtSDMPackageXML = [xml]$DtSDMPackageXML
+    $DtLocalName = $dt.LocalizedDisplayName
+    $DtCleanPath = ""
+    $DtCleanPath = $DtSDMPackageXML.AppMgmtDigest.DeploymentType.Installer.Contents.Content.Location[0]
+    $replacementString = "\\frmndfsvm11\repository$"
+
+    # check if array returned only a "\" value, indicating a singular value
+    if ($DtPath.Length -lt 2) {
+      $DtPath = $DtSDMPackageXML.AppMgmtDigest.DeploymentType.Installer.Contents.Content.Location
+      $DtPath = $DtPath.substring(28)
+    }
+    Write-Host "Current source: $DtPath" -ForegroundColor Cyan
+     $NewDtPath = $replacementString + $DtPath 
+
+    Write-Host "New directory source: $NewDtPath" -ForegroundColor Green
+    if ($NewDtPath -ne "") {
+      if ($NewDtPath.ToLower() -ne $DtPath.ToLower()) {
+        Set-CMDeploymentType -ApplicationName "$AppName" -DeploymentTypeName $DtLocalName -MsiOrScriptInstaller -ContentLocation "$NewDtPath"
+        Write-Host "Updating: $DtLocalName" -ForegroundColor Cyan
+      }
+      else {
+        Write-Host "No changes made" -ForegroundColor Cyan
+      }
+    }
+  }
+  Write-Host "--------------"
+  $RowCount++
+}
+
+
---
+++ content-location-packages.ps1
@@ -0,0 +1,26 @@
+﻿
+
+
+$pkgs = Get-CMPackage
+
+foreach ($pkg in $pkgs) {
+  Write-Host "PackageID: $($pkg.PackageID)"
+  Write-Host "Name: $($pkg.Name)"
+  $oldpath = $pkg.PkgSourcePath
+  Write-Host "Old Source: $oldpath"
+  $replacementString = "\\frmndfsvm11\repository$"
+  $oldpath = $oldpath.substring(28)
+    }
+    Write-Host "Current source: $oldpath" -ForegroundColor Cyan
+     $NewPath = $replacementString + $oldpath
+
+  Write-Host "New Source: $NewPath"
+  if ($NewPath -ne "") {
+    if ($NewPath.ToLower() -ne $oldpath.ToLower()) {
+      Write-Host "updating pacakge source path..."
+      Set-CMPackage -Id $pkg.PackageID -Path $NewPath
+    }
+}
+  write-host "----"
+
+
